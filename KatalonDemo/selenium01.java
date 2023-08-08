39bd39ffc8561f3ec417225ea50927b361019f43
sanjaysah <sanjaysah.engg@gmail.com>
2023-08-08 22:02:35 +0530

    Update selenium01.java

--- KatalonDemo/selenium01.java
+++ KatalonDemo/selenium01.java
@@ -21,6 +21,20 @@ public class KatalonDemo {
         driver.findElement(By.id("btn-login")).submit();
       //Assertion Third page
         Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/#appointment");
+
+      //Appointment Page
+        WebElement dropdown = driver.findElement(By.id("combo_facility"));
+        Select select = new Select(dropdown);
+        select.selectByVisibleText("Seoul CURA Healthcare Center");
+        driver.findElement(By.id("chk_hospotal_readmission")).click();
+        driver.findElement(By.id("radio_program_medicare")).click();
+        driver.findElement(By.id("txt_visit_date")).sendKeys("04/09/2023");
+        driver.findElement(By.id("txt_comment")).sendKeys("I want to get better!!");
+        driver.findElement(By.id("btn-book-appointment")).submit();
+    // Assert Appointment message
+        String appointmentconfirmation = driver.findElement(By.xpath("//h2[text()='Appointment Confirmation']")).getText();
+        Assert.assertEquals(appointmentconfirmation,"Appointment Confirmation");
+        
         driver.quit();
     }
 }
